Fixed error in calculation of JA, JB, JE, JG for p>1
The error was due to wrong reshaping order.

To load date from matlab:
from scipy.io import load_mat
tut = loadmat('tut.mat')
ut = tut['u'].swapaxes(1,2)[:,None]
u = tut['u'].ravel('F')
yt = tut['y'].transpose(0,3,2,1)
lines = tut['lines'].squeeze() - 1

examples/pnlss/nlss.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.linalg import norm
 
+from scipy.io import loadmat
+
 from pyvib.common import db
 from pyvib.forcing import multisine
 from pyvib.frf import covariance
@@ -42,14 +44,11 @@ p = 2
 # Construct model to estimate
 A = np.array([[0.73915535, -0.62433133],[0.6247377, 0.7364469]])
 B = np.array([[0.79287245], [-0.34515159]])
-if p == 1:
-    C = np.array([[0.71165154, 0.34917771]])
-    D = np.array([[0.04498052]])
-elif p == 2:
-    #C = np.array([[0.71165154, 0.34917771], [-0.001565625, -0.004855156]])
-    #D = np.array([[0.04498052],[0]])
-    C = np.array([[0.71165154, 0.34917771], [0.71165154, 0.34917771]])
-    D = np.array([[0.04498052],[0.04498052]])
+C = np.array([[0.71165154, 0.34917771]])
+D = np.array([[0.04498052]])
+if p == 2:
+    C = np.vstack((C,C))
+    D = np.vstack((D,0.1563532))
 
 Ffull = np.array([[-0.00867042, -0.00636662, 0.00197873, -0.00090865, -0.00088879,
                -0.02759694, -0.01817546, -0.10299409, 0.00648549, 0.08990175,
@@ -93,6 +92,10 @@ nlx = NLS([poly2y])
 nlx = NLS([poly2x])
 E = Efull[:,:len(nlx.nls)]
 
+E = Efull
+nlx = NLS([Pnlss(degree=[2,3], structure='full')])
+F = Ffull
+nly = NLS([Pnlss(degree=[2,3], structure='full')])
 
 true_model = NLSS(A, B, C, D, E, F)
 true_model.add_nl(nlx=nlx, nly=nly)
@@ -115,20 +118,16 @@ if True:
     # shape of u from multisine: (R,P*npp)
     u, lines, freq = multisine(N=npp, P=P, R=R, lines=kind, rms=RMSu)
     # if multiple input is required, this will copy u m times
-    
+
     # Transient: Add one period before the start of each realization. To generate
     # steady state data.
     T1 = np.r_[npp*Ntr, np.r_[0:(R-1)*P*npp+1:P*npp]]
     _, yorig, _ = true_model.simulate(u.ravel(), T1=T1)
+    print(norm(yorig))
     u = u.reshape((R,P,npp)).transpose((2,0,1))[:,None]  # (npp,m,R,P)
-    if p == 1:
-        y = yorig.reshape((R,P,npp)).transpose((2,0,1))[:,None]
-    elif p == 2:
-        # This works!
-        # y2 = yorig.reshape((R,P,npp,p)).transpose((2,3,0,1))
-        y = np.empty((npp,p,R,P))
-        y[:,0] = yorig[:,0].reshape((R,P,npp)).transpose((2,0,1))
-        y[:,1] = yorig[:,1].reshape((R,P,npp)).transpose((2,0,1))
+    y = yorig.reshape((R,P,npp,p),order='C').transpose((2,3,0,1))
+    # or in F order:
+    # y2 = yorig.reshape((npp,P,R,p),order='F').transpose((0,3,2,1))
     
     # Add colored noise to the output. randn generate white noise
     if add_noise:
@@ -157,7 +156,7 @@ if True:
     sig = Signal(uest,yest,fs=fs)
     sig.lines = lines
     # plot periodicity for one realization to verify data is steady state
-    sig.periodicity()
+    # sig.periodicity()
     # Calculate BLA, total- and noise distortion. Used for subspace identification
     sig.bla()
     # average signal over periods. Used for training of PNLSS model
@@ -172,6 +171,7 @@ if 'linmodel' not in locals() or True:
     linmodel = Subspace(sig)
     linmodel.estimate(2, 5, weight=weight)  # best model, when noise weighting is used
     linmodel.optimize(weight=weight)
+    
     print(f"Best subspace model, n, r: {linmodel.n}, {linmodel.r}")
     linmodel_orig = linmodel
 
@@ -199,24 +199,15 @@ poly3y = Polynomial(exponent=4,w=Wy)
 poly1x = Polynomial_x(exponent=2,w=[0,1])
 poly2x = Polynomial_x(exponent=3,w=[0,1])
 poly3x = Polynomial_x(exponent=4,w=[0,1])
-# nlx2 = NLS([poly1,poly2])  #,poly3])
-#nlx2 = NLS([poly2x,poly1y,poly3y])  #,poly3])
+
 nlx2 = NLS([poly1y,poly3y,poly2x,poly2y])  #,poly3])
-
 nlx2 = NLS([poly2x])
-# nlx2 = NLS([poly2y])  #,poly3])
 
-#nly2 = NLS([poly1x,poly2x])
-#nlx2 = NLS([poly2x, poly1y])
 nly2 = None
-#nlx2 = None
 
-#nlx2 = NLS([Pnlss(degree=[2,3], structure='full'), poly2y, poly1y])
-#nlx2 = NLS([Tanhdryfriction(eps=0.1, w=[1])])
-#nlx2 = NLS([Unilatteralspring(gap=gap, w=[1])])
 
-#nlx2 = NLS([Pnlss(degree=[2,3], structure='full')])
-#nly2 = NLS([Pnlss(degree=[2,3], structure='full')])
+nlx2 = NLS([Pnlss(degree=[2,3], structure='full')])
+nly2 = NLS([Pnlss(degree=[2,3], structure='full')])
 
 model = NLSS(linmodel)
 model.add_nl(nlx=nlx2, nly=nly2)
@@ -224,77 +215,84 @@ model.add_nl(nlx=nlx2, nly=nly2)
 #model.nlterms('y', [2,3], 'full')
 model.set_signal(sig)
 model.transient(T1)
-model.optimize(lamb=100, weight=weight, nmax=200)
+model.optimize(lamb=100, weight=weight, nmax=25)
+
+#raise SystemExit(0)
 
 # get best model on validation data. Change Transient settings, as there is
 # only one realization
 nl_errvec = model.extract_model(yval, uval, T1=npp)
 
 models = [linmodel, model]
-descrip = ('Linear', 'pnlss')  #, 'pnlss weight')
+descrip = [type(mod).__name__ for mod in models]
+descrip = tuple(descrip)  # convert to tuple for legend concatenation in figs
 # simulation error
-val = np.empty((len(models),len(uval)))
-est = np.empty((len(models),len(um)))
-test = np.empty((len(models),len(utest)))
+val = np.empty((*yval.shape, len(models)))
+est = np.empty((*ym.shape, len(models)))
+test = np.empty((*ytest.shape, len(models)))
 for i, model in enumerate(models):
-    test[i] = model.simulate(utest, T1=npp*Ntr)[1].T
-    val[i] = model.simulate(uval, T1=npp*Ntr)[1].T
-    est[i] = model.simulate(um, T1=T1)[1].T
+    test[...,i] = model.simulate(utest, T1=npp*Ntr)[1]
+    val[...,i] = model.simulate(uval, T1=npp*Ntr)[1]
+    est[...,i] = model.simulate(um, T1=T1)[1]
 
+# convenience inline functions
+stack = lambda ydata, ymodel: \
+    np.concatenate((ydata[...,None], (ydata[...,None] - ymodel)),axis=2)
 rms = lambda y: np.sqrt(np.mean(y**2, axis=0))
-est_err = np.hstack((ym, (ym.T - est).T))
-val_err = np.hstack((yval, (yval.T - val).T))
-test_err = np.hstack((ytest, (ytest.T - test).T))
+est_err = stack(ym, est)  # (npp*R,p,nmodels)
+val_err = stack(yval, val)
+test_err = stack(ytest, test)
 noise = np.abs(np.sqrt(Pest*covY.squeeze()))
 print(f"err for models {descrip}")
 print(f'rms error noise: {rms(noise)}\tdb: {db(rms(noise))} ')
-print(f'rms error est:\n    {rms(est_err[:,1:])}\ndb: {db(rms(est_err[:,1:]))}')
-print(f'rms error val:\n    {rms(val_err[:,1:])}\ndb: {db(rms(val_err[:,1:]))}')
-print(f'rms error test:\n    {rms(test_err[:,1:])}\ndb: {db(rms(test_err[:,1:]))}')
+print(f'rms error est:  \n{rms(est_err)}   \ndb: \n{db(rms(est_err))}')
+print(f'rms error val:  \n{rms(val_err)}   \ndb: \n{db(rms(val_err))}')
+print(f'rms error test: \n{rms(test_err)}  \ndb: \n{db(rms(test_err))}')
 
 
 ## Plots ##
 # store figure handle for saving the figures later
 figs = {}
 
-# linear and nonlinear model error
-plt.figure()
-plt.plot(est_err)
-plt.xlabel('Time index')
-plt.ylabel('Output (errors)')
-plt.legend(('Output',) + descrip)
-plt.title('Estimation results')
-figs['estimation_error'] = (plt.gcf(), plt.gca())
+# linear and nonlinear model error; plot for each output
+for pp in range(p):
+    plt.figure()
+    plt.plot(est_err[:,pp])
+    plt.xlabel('Time index')
+    plt.ylabel('Output (errors)')
+    plt.legend(('Output',) + descrip)
+    plt.title('Estimation results p:{pp}')
+    figs['estimation_error'] = (plt.gcf(), plt.gca())
 
-# result on validation data
-N = len(yval)
-freq = np.arange(N)/N*fs
-plottime = val_err
-plotfreq = np.fft.fft(plottime, axis=0)/np.sqrt(N)
-nfd = plotfreq.shape[0]
-plt.figure()
-plt.plot(freq[lines], db(plotfreq[lines]), '.')
-plt.plot(freq[lines], db(np.sqrt(Pest*covY[lines].squeeze() / N)), '.')
-plt.xlabel('Frequency')
-plt.ylabel('Output (errors) (dB)')
-plt.legend(('Output',) + descrip + ('Noise',))
-plt.title('Validation results')
-figs['val_data'] = (plt.gcf(), plt.gca())
+    # result on validation data
+    N = len(yval)
+    freq = np.arange(N)/N*fs
+    plottime = val_err
+    plotfreq = np.fft.fft(plottime, axis=0)/np.sqrt(N)
+    nfd = plotfreq.shape[0]
+    plt.figure()
+    plt.plot(freq[lines], db(plotfreq[lines,pp]), '.')
+    plt.plot(freq[lines], db(np.sqrt(Pest*covY[lines,pp,pp].squeeze() / N)), '.')
+    plt.xlabel('Frequency')
+    plt.ylabel('Output (errors) (dB)')
+    plt.legend(('Output',) + descrip + ('Noise',))
+    plt.title('Validation results p:{pp}')
+    figs['val_data'] = (plt.gcf(), plt.gca())
 
-# result on test data
-N = len(ytest)
-freq = np.arange(N)/N*fs
-plottime = test_err
-plotfreq = np.fft.fft(plottime, axis=0)/np.sqrt(N)
-nfd = plotfreq.shape[0]
-plt.figure()
-plt.plot(freq[:nfd//2], db(plotfreq[:nfd//2]), '.')
-plt.plot(freq[:nfd//2], db(np.sqrt(Pest*covY[:nfd//2].squeeze() / N)), '.')
-plt.xlabel('Frequency')
-plt.ylabel('Output (errors) (dB)')
-plt.legend(('Output',) + descrip + ('Noise',))
-plt.title('Test results')
-figs['test_data'] = (plt.gcf(), plt.gca())
+    # result on test data
+    N = len(ytest)
+    freq = np.arange(N)/N*fs
+    plottime = test_err
+    plotfreq = np.fft.fft(plottime, axis=0)/np.sqrt(N)
+    nfd = plotfreq.shape[0]
+    plt.figure()
+    plt.plot(freq[:nfd//2], db(plotfreq[:nfd//2,pp]), '.')
+    plt.plot(freq[:nfd//2], db(np.sqrt(Pest*covY[:nfd//2,pp,pp].squeeze() / N)), '.')
+    plt.xlabel('Frequency')
+    plt.ylabel('Output (errors) (dB)')
+    plt.legend(('Output',) + descrip + ('Noise',))
+    plt.title('Test results p:{pp}')
+    figs['test_data'] = (plt.gcf(), plt.gca())
 
 # optimization path for PNLSS
 plt.figure()

pyvib/common.py
@@ -239,7 +239,7 @@ def normalize_columns(mat):
     return mat/scaling, scaling
 
 def lm(fun, x0, jac, info=2, nmax=50, lamb=None, ftol=1e-8, xtol=1e-8,
-       gtol=1e-8, args=(), kwargs={}):
+       gtol=1e-8, cost_normalize=None, args=(), kwargs={}):
     """Solve a nonlinear least-squares problem using levenberg marquardt
        algorithm. See also :scipy-optimize:func:`scipy.optimize.least_squares`
 
@@ -288,6 +288,14 @@ def lm(fun, x0, jac, info=2, nmax=50, lamb=None, ftol=1e-8, xtol=1e-8,
     x0_mat[0] = x0.copy()
     cost_vec[0] = cost.copy()
 
+    # Allow for different kinds of cost_normalization
+    if cost_normalize is None:
+        _cost_normalize = lambda x: x
+    elif np.isscalar(cost_normalize):
+        _cost_normalize = lambda x: x/cost_normalize
+    else:  # if callable(obj)
+        _cost_normalize = cost_normalize
+
     if info == 2:
         print(f"{'i':3} | {'inner':5} | {'cost':12} | {'cond':12} |"
               f" {'lambda':6}")
@@ -326,7 +334,6 @@ def lm(fun, x0, jac, info=2, nmax=50, lamb=None, ftol=1e-8, xtol=1e-8,
             x0test = x0 + ds
             err = fun(x0test, *args, **kwargs)
             cost = np.dot(err,err)
-
             if cost >= cost_old:
                 # step unsuccessful, increase lambda, ie. Lean more towards
                 # gradient descent method(converges in larger range)
@@ -352,8 +359,8 @@ def lm(fun, x0, jac, info=2, nmax=50, lamb=None, ftol=1e-8, xtol=1e-8,
         if info == 2:
             jac_cond = sr[0]/sr[-1]
             # {cost/2/nfd/R/p:12.3f} for freq weighting
-            print(f"{niter:3d} | {ninner:5d} | {cost:12.8g} | {jac_cond:12.3f}"
-                  f" | {lamb:6.3f}")
+            print(f"{niter:3d} | {ninner:5d} | {_cost_normalize(cost):12.8g}"
+                  f" | {jac_cond:12.3f} | {lamb:6.3f}")
 
         if cost < cost_old or stop:
             cost_old = cost
@@ -371,8 +378,9 @@ def lm(fun, x0, jac, info=2, nmax=50, lamb=None, ftol=1e-8, xtol=1e-8,
     message = TERMINATION_MESSAGES[status]
     if info > 0:
         print(f"Terminated: {message:s}")
-        print(f"Function evaluations {nfev}, initial cost {cost_vec[0]:.4e}, "
-              f"final cost {cost:.4e}")
+        print(f"Function evaluations {nfev}, "
+              f"initial cost {_cost_normalize(cost_vec[0]):.4e}, "
+              f"final cost {_cost_normalize(cost):.4e}")
 
     res = {'x':x0, 'cost': cost, 'fun':err, 'niter': niter, 'x_mat':
            x0_mat[:niter], 'cost_vec':cost_vec[niter], 'message':message,

pyvib/nlss.py
@@ -33,9 +33,8 @@ class NLSS(NonlinearStateSpace, StateSpaceIdent):
             kwargs['dt'] = 1  # unit sampling
 
         super().__init__(*sys, **kwargs)
-
         self.nlx = NLS()
-        self.nly = NLS() 
+        self.nly = NLS()
 
     def set_signal(self,signal):
         self.signal = signal
@@ -241,23 +240,23 @@ def jacobian(x0, system, weight=False):
     # reshape so first row of JA is the derivative wrt all elements in A for
     # first time step, first putput, then second output, then next time,...
     JA = element_jacobian(x_trans, A_Edhdx, Gdidy, C_Fdjdx, np.arange(n**2))
-    JA = JA.transpose((2,0,1)).reshape((nts*p, n**2))
+    JA = JA.transpose((2,0,1)).reshape((nts*p, n**2),order='F')
     JA = JA[system.idx_remtrans]  # (p*ns,n**2)
 
     JB = element_jacobian(u_trans, A_Edhdx, Gdidy, C_Fdjdx, np.arange(n*m))
-    JB = JB.transpose((2,0,1)).reshape((nts*p, n*m))
+    JB = JB.transpose((2,0,1)).reshape((nts*p, n*m),order='F')
     JB = JB[system.idx_remtrans]  # (p*ns,n*m)
 
     if system.nlx.xactive.size:
         JE = element_jacobian(hvec, A_Edhdx, Gdidy, C_Fdjdx, system.nlx.xactive)
-        JE = JE.transpose((2,0,1)).reshape((nts*p, len(system.nlx.xactive)))
+        JE = JE.transpose((2,0,1)).reshape((nts*p, len(system.nlx.xactive)), order='F')
         JE = JE[system.idx_remtrans]  # (p*ns,nactiveE)
     else:
         JE = np.array([]).reshape(p*ns,0)
 
     if system.nlx.yactive.size:
         JG = element_jacobian(ivec, A_Edhdx, Gdidy, C_Fdjdx, system.nlx.yactive)
-        JG = JG.transpose((2,0,1)).reshape((nts*p, len(system.nlx.yactive)))
+        JG = JG.transpose((2,0,1)).reshape((nts*p, len(system.nlx.yactive)), order='F')
         JG = JG[system.idx_remtrans]  # (p*ns,nactiveE)
     else:
         JG = np.array([]).reshape(p*ns,0)

pyvib/statespace.py
@@ -374,6 +374,9 @@ class NonlinearStateSpace(StateSpace):
 class StateSpaceIdent():
     def __init__(self):
         self._weight = None
+        if not hasattr(self, '_cost_normalize'):
+            self._cost_normalize = 1
+
 
     def cost(self, x0=None, weight=False):
         if weight is True:
@@ -393,6 +396,15 @@ class StateSpaceIdent():
         self.freq_weight = True
         if weight is False:
             self.freq_weight = False
+            
+        if isinstance(self,NonlinearStateSpace) and not self.freq_weight:
+            # HACK. We want the cost function to show as rms. Ideally optimize
+            # took a signal to use for optimizing in. For now this will do
+            nt =  self.signal.ym.size  # (nt*p)
+            self._cost_normalize = lambda x: np.sqrt(x/nt)
+        if isinstance(self,NonlinearStateSpace) and self.freq_weight:
+            # should be weighted mean square cost function (divided by 2*NFD*R*p) 
+            pass
 
         if info:
             print(f'\nStarting {self.__class__.__name__} optimization')
@@ -402,7 +414,7 @@ class StateSpaceIdent():
         if method is None:
             res = lm(fun=self.costfcn, x0=x0, jac=self.jacobian, info=info,
                      nmax=nmax, lamb=lamb, ftol=ftol, xtol=xtol, gtol=gtol,
-                     kwargs=kwargs)
+                     cost_normalize=self._cost_normalize, kwargs=kwargs)
         else:
             res = least_squares(self.costfcn, x0, self.jacobian, method='lm',
                                 x_scale='jac', kwargs=kwargs)
@@ -462,7 +474,7 @@ def costfcn_time(x0, system, weight=False):
     # T2 = system.T2
     # p is the actual number of output in the signal, not the system output
     R, p, npp = system.signal.R, system.signal.p, system.signal.npp
-    p = system.p
+    # p = system.p
     nfd = npp//2
     # without_T2 = system.without_T2
 
@@ -476,10 +488,10 @@ def costfcn_time(x0, system, weight=False):
     t_mod, y_mod, x_mod = system.simulate(system.signal.um)
 
     # Compute the (weighted) error signal without transient
-    if system.signal._ydm is not None:
-        ym = np.hstack((system.signal.ym, system.signal._ydm))
-    else:
-        ym = system.signal.ym
+#    if system.signal._ydm is not None:
+#        ym = np.hstack((system.signal.ym, system.signal._ydm))
+#    else:
+    ym = system.signal.ym
 
     err = y_mod - ym  #[without_T2, :p] - system.signal.ym[without_T2]
     if weight is not False and system.freq_weight: